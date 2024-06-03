3f5aff4e280b5b8ec649d331a23f091daaf1562b
USER <USER@moderato-85246>
2024-06-03 10:57:31 +0900

    first

---
+++ BatchDefinitiveGuideMaven/src/main/java/batch/guide/BatchDefinitiveGuideMavenApplication.java
@@ -0,0 +1,13 @@
+package batch.guide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchDefinitiveGuideMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchDefinitiveGuideMavenApplication.class, args);
+	}
+
+}
